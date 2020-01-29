da922292ac0c66e1a0f48f3f8e6060c64591859d
janiello <janiello417@gmail.com>
2020-01-29 13:17:16 -0600

    Completely altered code for simpler functionality and fewer keypress events. Changed scope of most variables to display everything on page load and allow the user to immediately start guessing letters instead of pressing space to start, then being able to guess. Still need to figure out how to hide the word with blanks and correctly loop through the word to check against user input and properly display correct guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,49 @@
-// First I need a list of words.
-var wordList = ["westeros", "stark", "dragons", "bravos", "targaryen", "wildlings", "khaleesi", "lannister", "winterfell", "baratheon"];
+// First I need a list of words
+var wordList = ["WETSEROS", "STARK", "DRAGONS", "BRAVOS", "TARGARYEN", "WILDLINGS", "KHALEESI", "LANNISTER", "WINTERFELL", "BARATHEON"];
 
-// Generate a random word from the array of available words on the keypress event. If this is called in the global scope, it will only work once on page load and the event will simply reveal the already generated word.
-var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-
-// Hide the word with "_ _ _ _ _" until the corect letters are guessed.
-var blanks = " _ "
-var hiddenWord = randomWord.replace(/[a-z]/g, blanks);
-
-// Assign the HTML elements where I want certain things to appear (or disappear) to different variables.
+// Assign the HTML elements where I want certain things to appear (or disappear) to different variables
 var word = document.getElementById("word");
 var winsText = document.getElementById("wins");
 var guessesText = document.getElementById("guesses");
 var lettersText = document.getElementById("letters");
-var directions = document.getElementById("main-menu");
 
 // Assign the variables to the number of wins the user has and guesses left
 var wins = 0;
 var guesses = 10;
 
+// Generate a random word from the array of available words on page load
+var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+// Display that random word in the correct HTML element, along with wins, number of guesses, and letters guessed info, and log the actual word in the console
+word.textContent = randomWord;
+winsText.textContent = "Wins: " + wins;
+guessesText.textContent = "Number of Guesses Left: " + guesses;
+lettersText.textContent = "Letters Already Guessed: ";
+console.log(randomWord);
+
+// Hide the word with "_ _ _ _ _" until the corect letters are guessed.
+/*var blanks = " _ "
+var hiddenWord = randomWord.replace(/[a-z]/g, blanks);*/
+
+
 // Create an empty array which will ultimately contain incorrectly guessed letters
 var lettersGuessed = [];
 
-// Create a keypress event for the "start game" functionlity
+// Create a keypress event for the "start game" and letter guessing functionlity
 document.onkeyup = function(event) {
-    // Make the event work only when a specific key is pressed
-    if (event.code == "Space") {
-        word.textContent = hiddenWord;
-        // Hide the main menu, display wins, number of guesses, and letters guessed info, and log the actual word in the console
-        directions.textContent = "";
-        winsText.textContent = "Wins: " + wins;
-        guessesText.textContent = "Number of Guesses Left: " + guesses;
-        lettersText.textContent = "Letters Already Guessed: " + lettersGuessed;
-        console.log(randomWord);
-    } else if (event.code != "Space") {
-        // I need a variable that takes in whatever letter the user guesses
-        var userGuess = event.key;
-        // console.log(userGuess);
+    // I need a variable that takes in whatever letter the user guesses
+    var userGuess = event.key.toUpperCase();
+    console.log(userGuess)
+    // Loop through the random word to check against the userGuess
+    for (var i = 0; i < randomWord.length; i++) {
         // If the user guesses correctly, change the corresponding blank space to the correctly geussed letter
-        for (var i = 0; i < randomWord.length; i++) {
-            if (userGuess === randomWord[i]) {
-                
-            } else {
-                console.log("Try again");
-            };
-        }
-        // This currently logs to the console for each letter (up to 10 times per guess)
+        if (userGuess === randomWord[i]) {
+            console.log("correct");
+        } else {
         // If the user guesses incorrectly, push the guessed letter to the lettersGuessed array
-
+            console.log("try again");
         // And reduce the number of guesses by 1 using a for loop and counter variables
+        }
     };
 };
 
